492f4b1a7cc3b21b0e60a46e4e8242ccb67f2dbc
Prateek Bhatnagar <prateek89born@gmail.com>
2018-08-08 22:00:23 -0700

    logger only for dev env

--- src/components/store.js
+++ src/components/store.js
@@ -5,16 +5,17 @@ import thunk from 'redux-thunk';
 
 const reducer = combineReducers(reducers);
 const logger = createLogger({
-  predicate: (getState, action) => action.type !== false
+	predicate: (getState, action) => action.type !== false
 });
 
 let middlewares = [thunk];
-
-middlewares.push(logger);
+if (process.env.NODE_ENV === `development`) {
+	middlewares.push(logger);
+}
 
 const store = createStore(
-  reducer,
-  applyMiddleware(...middlewares)
+	reducer,
+	applyMiddleware(...middlewares)
 );
 
 export default store;
