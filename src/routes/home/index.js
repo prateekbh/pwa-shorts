127c82775af1ebb69507b7e80274fa3d94f4f521
Prateek Bhatnagar <prateek89born@gmail.com>
2019-06-12 23:53:50 -0700

    useHooks

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,4 +1,5 @@
 import { h, Component } from 'preact';
+import {useEffect} from 'preact/hooks';
 import { connect } from 'preact-redux';
 import Snackbar from 'preact-material-components/Snackbar';
 import 'preact-material-components/Snackbar/style.css';
@@ -6,22 +7,21 @@ import style from './style';
 import { fetchNews } from '../../actions/actions';
 import NewsTile from './news-tile';
 
-class Home extends Component {
-	componentDidMount() {
+const Home = (props) => {
+	useEffect(() => {
+		console.log('hi')
 		this.props.dispatch(fetchNews());
-	}
-	render(props) {
-		return (
-			<div class={style.home}>
-				{props.reducer.news.map(news => <NewsTile news={news} />)}
-				<Snackbar ref={bar => {
-					this.bar=bar;
-					window.snackbar = bar;
-				}}
-				/>
-			</div>
-		);
-	}
+	}, []);
+	return (
+		<div class={style.home}>
+			{props.reducer.news.map(news => <NewsTile news={news} />)}
+			<Snackbar ref={bar => {
+				this.bar=bar;
+				window.snackbar = bar;
+			}}
+			/>
+		</div>
+	);
 }
 
 export default connect((state) =>
